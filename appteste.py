721cf57a36946135e3cf392489279afc9dd7345a
alunocanva25-hub <alunocanva25@gmail.com>
2025-12-28 02:23:59 -0300

    Update appteste.py

--- appteste.py
+++ appteste.py
@@ -476,7 +476,7 @@ def acumulado_mensal_fig_e_tabela(df_base, col_data):
     # =========================
     # Ajustes finos aqui:
     x_leg = - 0.10
-    y_leg = - 0.29
+    y_leg = - 0.33
     dy = 0.055
 
     fig.add_annotation(
